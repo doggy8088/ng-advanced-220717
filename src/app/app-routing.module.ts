f6c8cf3a3749b66f3f4077aa16cbd590c8aec6c7
Will <doggy.huang@gmail.com>
2022-07-17 15:15:36 +0800

    建立 UtilitiesModule 延遲載入的骨架

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
-import { ColorsComponent } from './utilities/colors/colors.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -18,13 +17,17 @@ const routes: Routes = [
       { path: 'dashboard', component: DashboardComponent, title: 'SB Admin 2 - Dashboard' },
       { path: 'page1', component: Page1Component, title: 'SB Admin 2 - Page 1' },
       { path: 'page2', component: Page2Component, title: 'SB Admin 2 - Page 2' },
-      {
-        path: 'utilities',
-        children: [
-          { path: 'colors/:type', component: ColorsComponent, data: { key: 'value' } },
-        ]
-      }
+      // {
+      //   path: 'utilities',
+      //   children: [
+      //     { path: 'colors/:type', component: ColorsComponent, data: { key: 'value' } },
+      //   ]
+      // }
     ]
+  },
+  {
+    path: 'utilities',
+    loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule)
   },
   // { path: '**', component: NotFoundComponent },
 ];
