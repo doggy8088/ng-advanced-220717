985d11001f06af9befe908eb64b07720205358a6
Will <doggy.huang@gmail.com>
2022-07-17 16:29:41 +0800

    示範 PreloadAllModules 用法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
@@ -28,7 +28,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    useHash: true
+    useHash: true,
+    preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
 })
