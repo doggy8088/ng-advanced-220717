a88c2b42fd923ab53a9e062f3325cc9294d5843c
Will <doggy.huang@gmail.com>
2022-07-24 16:23:57 +0800

    處理變動欄位數量的表單如何初始化 FormModel

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './login2.component.html',
-  styleUrls: ['./login2.component.css']
+  styleUrls: ['./login2.component.css'],
 })
 export class Login2Component implements OnInit {
   data = {
     email: 'user@example.com',
     password: '123123',
-    // city: 'Taipei',
-    isRememberMe: true
+    isRememberMe: true,
+    profiles: [
+      {
+        city: 'Taipei',
+        tel: '0988-888888',
+      },
+      {
+        city: '台中',
+        tel: '0944-444444',
+      },
+      {
+        city: 'Kaoshuang',
+        tel: '0911111111',
+      },
+    ],
   };
 
   orig_body_className = document.body.className;
@@ -19,34 +39,46 @@ export class Login2Component implements OnInit {
   form = this.fb.group({
     email: this.fb.nonNullable.control('', {
       validators: [Validators.required, Validators.email],
-      updateOn: 'blur'
+      updateOn: 'blur',
     }),
     password: this.fb.control('', {
-      validators: [Validators.required, Validators.minLength(6), Validators.maxLength(32)]
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(32),
+      ],
     }),
-    isRememberMe: this.fb.control(true, {
-    }),
+    isRememberMe: this.fb.control(true, {}),
     profiles: this.fb.array([
       this.makeProfile('Taipei', '0988-888888'),
-      this.makeProfile('台中', '0944-444444')
-    ])
+      this.makeProfile('台中', '0944-444444'),
+    ]),
   });
 
   makeProfile(city: string, tel: string) {
     return this.fb.group({
       city: this.fb.control(city, { validators: [Validators.required] }),
-      tel: this.fb.control(tel, { validators: [Validators.required] })
-    })
+      tel: this.fb.control(tel, { validators: [Validators.required] }),
+    });
   }
 
-  constructor(private router: Router, private route: ActivatedRoute, private fb: FormBuilder) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     document.body.className = 'bg-gradient-primary';
 
     setTimeout(() => {
-      // this.form.setValue(this.data);
-      this.form.patchValue(this.data);
+
+      this.form.controls.profiles.clear();
+      this.data.profiles.forEach(profile => {
+        this.form.controls.profiles.push(this.makeProfile(profile.city, profile.tel));
+      });
+      this.form.setValue(this.data);
+      // this.form.patchValue(this.data);
     }, 2000);
   }
 
