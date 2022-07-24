c9720d51c74098bf00651db5e274195f7fe060ac
Will <doggy.huang@gmail.com>
2022-07-24 16:51:51 +0800

    更新 forbiddenPassword 邏輯，只要包含特定文字就不能當密碼

--- src/app/login2/forbiddenPassword.ts
+++ src/app/login2/forbiddenPassword.ts
@@ -3,7 +3,7 @@ import { AbstractControl } from '@angular/forms';
 export function forbiddenPassword(control: AbstractControl) {
   if (!control.value) { return null; }
   let words = ['will', 'duotify', '123'];
-  var result = words.includes(control.value);
+  var result = words.some(x => control.value.indexOf(x) >= 0);
   if (result) {
     return { forbiddenPassword: true };
   } else {
