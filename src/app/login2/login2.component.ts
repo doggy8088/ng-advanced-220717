fadc898b0840385569d6432e5bdde882a85cda3c
Will <doggy.huang@gmail.com>
2022-07-24 14:16:42 +0800

    示範弱型別的 binding 方法

--- src/app/login2/login2.component.ts
+++ src/app/login2/login2.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, UntypedFormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, UntypedFormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -25,6 +25,18 @@ export class Login2Component implements OnInit {
 
   ngOnDestroy(): void {
     document.body.className = this.orig_body_className;
+  }
+
+  fc(name: string) {
+    return this.form.get(name) as FormControl;
+  }
+
+  fg(name: string) {
+    return this.form.get(name) as FormGroup;
+  }
+
+  fa(name: string) {
+    return this.form.get(name) as FormArray;
   }
 
   // doLogin(form: NgForm) {
